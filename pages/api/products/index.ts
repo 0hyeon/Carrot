23e04b961168ba7188853533ebb4650f044c3393
0hyeon <djdjdjk2006@naver.com>
2022-10-07 18:23:02 +0900

    See Products

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -9,32 +9,42 @@ async function handler(
 ) {
   //   const { name, price, description } = req.body;
   //   const { user } = req.session;
-  const {
-    body: { name, price, description },
-    session: { user },
-  } = req;
-  const product = await client.product.create({
-    data: {
-      name,
-      price: +price,
-      description,
-      image: "xx",
-      user: {
-        connect: {
-          id: user?.id,
+  if (req.method === "GET") {
+    const products = await client.product.findMany({});
+    return res.json({
+      ok: true,
+      products,
+    });
+  }
+
+  if (req.method === "POST") {
+    const {
+      body: { name, price, description },
+      session: { user },
+    } = req;
+    const product = await client.product.create({
+      data: {
+        name,
+        price: +price,
+        description,
+        image: "xx",
+        user: {
+          connect: {
+            id: user?.id,
+          },
         },
       },
-    },
-  });
-  res.json({
-    ok: true,
-    product,
-  });
+    });
+    res.json({
+      ok: true,
+      product,
+    });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    method: "POST",
+    methods: ["GET", "POST"],
     handler,
   })
 );
