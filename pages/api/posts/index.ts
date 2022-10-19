0faee40ef8e09ab51bcd5b904722f54565b04e1f
0hyeon <djdjdjk2006@naver.com>
2022-10-19 14:25:35 +0900

    Geo search

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -6,11 +6,11 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const {
-    body: { question, latitude, longitude },
-    session: { user },
-  } = req;
   if (req.method === "POST") {
+    const {
+      body: { question, latitude, longitude },
+      session: { user },
+    } = req;
     const post = await client.post.create({
       data: {
         question,
@@ -29,6 +29,11 @@ async function handler(
     });
   }
   if (req.method === "GET") {
+    const {
+      query: { latitude, longitude },
+    } = req;
+    const parsedLatitude = parseFloat(String(latitude));
+    const parsedLongitude = parseFloat(String(longitude));
     const posts = await client.post.findMany({
       include: {
         user: {
@@ -43,6 +48,16 @@ async function handler(
             wondering: true,
             answers: true,
           },
+        },
+      },
+      where: {
+        latitude: {
+          gte: parsedLatitude - 0.01,
+          lte: parsedLatitude + 0.01,
+        },
+        longitude: {
+          gte: parsedLongitude - 0.01,
+          lte: parsedLongitude + 0.01,
         },
       },
     });
