0cbd8e02ae1d3d3ab02edcfc2e29c2d879c9cf5b
0hyeon <djdjdjk2006@naver.com>
2022-10-24 21:17:28 +0900

    See Messages

--- pages/api/streams/[id]/index.ts
+++ pages/api/streams/[id]/index.ts
@@ -2,7 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
@@ -10,18 +9,28 @@ async function handler(
   const {
     query: { id },
   } = req;
-
-  const stream = client.stream.findUnique({
+  const stream = await client.stream.findUnique({
     where: {
       id: Number(id),
     },
+    include: {
+      messages: {
+        select: {
+          id: true,
+          message: true,
+          user: {
+            select: {
+              avatar: true,
+              id: true,
+            },
+          },
+        },
+      },
+    },
   });
-  res.json({
-    ok: true,
-    stream,
-  });
+  if (!stream) return res.status(400).json({ ok: false });
+  res.json({ ok: true, stream });
 }
-
 export default withApiSession(
   withHandler({
     methods: ["GET"],
--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -4,22 +4,37 @@ import Message from "@components/message";
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import { Stream } from "@prisma/client";
-import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import useMutation from "@libs/client/useMutation";
+import useUser from "@libs/client/useUser";
+import { useEffect } from "react";
+
+interface StreamMessage {
+  message: string;
+  id: number;
+  user: {
+    avatar?: string;
+    id: number;
+  };
+}
+
+interface StreamWithMessages extends Stream {
+  messages: StreamMessage[];
+}
 
 interface StreamResponse {
-  ok: boolean;
-  stream: Stream;
+  ok: true;
+  stream: StreamWithMessages;
 }
+
 interface MessageForm {
   message: string;
 }
-
 const Stream: NextPage = () => {
+  const { user } = useUser();
   const router = useRouter();
   const { register, handleSubmit, reset } = useForm<MessageForm>();
-  const { data } = useSWR<StreamResponse>(
+  const { data, mutate } = useSWR<StreamResponse>( //GET
     router.query.id ? `/api/streams/${router.query.id}` : null
   );
   const onValid = (form: MessageForm) => {
@@ -28,10 +43,16 @@ const Stream: NextPage = () => {
     sendMessages(form);
   };
   const [sendMessages, { loading, data: sendMessageData }] = useMutation(
+    //POST
     `/api/streams/${router.query.id}/messages`
   );
   useEffect(() => {
-    if (data?.ok === false) {
+    if (sendMessageData && sendMessageData.ok) {
+      mutate(); // 다시 data refetch
+    }
+  }, [sendMessageData, mutate]);
+  useEffect(() => {
+    if (data?.ok !== true) {
       router.push("/streams");
     }
   }, [data, router]);
@@ -51,9 +72,16 @@ const Stream: NextPage = () => {
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Live Chat</h2>
           <div className="py-10 pb-16 h-[50vh] overflow-y-scroll  px-4 space-y-4">
-            <Message message="Hi how much are you selling them for?" />
+            {/* <Message message="Hi how much are you selling them for?" />
             <Message message="I want ￦20,000" reversed />
-            <Message message="미쳤어" />
+            <Message message="미쳤어" /> */}
+            {data?.stream?.messages.map((message) => (
+              <Message
+                key={message.id}
+                message={message.message}
+                reversed={message.user.id === user?.id}
+              />
+            ))}
           </div>
           <div className="fixed py-2 bg-white bottom-0 inset-x-0">
             <form
