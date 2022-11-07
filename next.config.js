93c886f08480491ca3dd60e1e632f151daa9f3f5
0hyeon <djdjdjk2006@naver.com>
2022-11-07 22:29:18 +0900

    Video and Thumbnails

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["imagedelivery.net"],
+    domains: ["imagedelivery.net", "videodelivery.net"],
   },
 };
 
